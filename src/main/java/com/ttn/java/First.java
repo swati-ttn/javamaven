0dec356298798c012fdab5e845fd67200a2cd6c2
Swati TTN <swatiaswal02@gmail.com>
2021-10-02 15:26:57 +0530

    Java maven project first commit

---
+++ src/main/java/com/ttn/java/First.java
@@ -0,0 +1,23 @@
+package com.ttn.java;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.text.*;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hi, com.ttn.java.First Line through Maven");
+
+        /*
+        String name = "Something";
+
+        if(name!=null && !name.equalsIgnoreCase("")) {
+            System.out.println("String is non-null");
+        }
+
+        if(StringUtils.isNotBlank(name)) {
+            System.out.println("StringUtils - String is non-null");
+        }
+
+         */
+    }
+}
